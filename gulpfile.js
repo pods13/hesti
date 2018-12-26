4532ab03ff67ea01ef7671ad92bb8bd7f35bd80b
pods13 <alta0815@yandex.ru>
2018-12-27 01:58:05 +0300

    Config gulp to support changes in js

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,18 @@ const nodeSassMagicImporter = require('node-sass-magic-importer');
 
 const stylesSource = 'src/css/**/*.scss';
 const stylesDestDirectory = 'static/assets/css';
+const jsSource = 'src/js/**/*.js';
+const jsDestDirectory = 'static/assets/js';
 
-gulp.task('clean:styles', function (done) {
-	rimraf.sync(`${stylesDestDirectory}/*`);
-	done();
-});
+function removeFilesFromDirectory(dir) {
+	console.log('invoked')
+	return function(cb) {
+		rimraf.sync(`${dir}/*`);
+		cb();
+	}
+}
+
+gulp.task('clean:styles', removeFilesFromDirectory(stylesDestDirectory));
 
 gulp.task('styles', gulp.series('clean:styles', () => {
 	return gulp.src(stylesSource)
@@ -23,10 +30,23 @@ gulp.task('styles', gulp.series('clean:styles', () => {
 		.pipe(gulp.dest(stylesDestDirectory));	
 }));
 
-gulp.task('build', gulp.series('styles'));
-
-gulp.task('watch', () => {
+gulp.task('watch:styles', () => {
 	return gulp.watch(stylesSource, gulp.series('styles'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'styles'));
+gulp.task('clean:js', removeFilesFromDirectory(jsDestDirectory));
+
+gulp.task('js', gulp.series('clean:js', () => {
+	return gulp.src(jsSource)
+		.pipe(gulp.dest(jsDestDirectory));	
+}));
+
+gulp.task('watch:js', () => {
+	return gulp.watch(jsSource, gulp.series('js'));
+});
+
+gulp.task('watch', gulp.parallel('watch:styles', 'watch:js'));
+
+gulp.task('build', gulp.parallel('styles', 'js'));
+
+gulp.task('default', gulp.parallel('watch', 'styles', 'js'));
