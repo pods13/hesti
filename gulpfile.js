52ab63ed4fa455d5da1b5d28aa0876a729781afe
pods13 <alta0815@yandex.ru>
2019-06-24 01:03:36 +0300

    Added vendor:scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,19 @@ const { join } = require('path');
 const pathToPackage  = join(process.env.INIT_CWD, 'package.json');
 const { gulpConfig } = require(pathToPackage);
 
-console.log(gulpConfig);
+const loadPlugins = require('gulp-load-plugins');
+const $ = loadPlugins({
+	pattern: ['*'],
+	scope: ['dependencies']
+});
+
+const gulp = $.gulp;
+
+const jsAssets = join(process.env.INIT_CWD, 'static/assets/js');
+
+gulp.task('vendor:scripts', () => {
+	return gulp.src(['node_modules/jquery/dist/jquery.slim.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+		.pipe(gulp.dest(jsAssets));
+});
+
+gulp.task('default', gulp.series('vendor:scripts'));
