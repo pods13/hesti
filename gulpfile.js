4d0793990d76249c4d177946d596a0893988c84a
pods13 <alta0815@yandex.ru>
2019-06-24 00:32:43 +0300

    gulp file from the ground up

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,5 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const rimraf = require('rimraf');
-const autoprefixer = require('gulp-autoprefixer');
+const { join } = require('path');
+const pathToPackage  = join(process.env.INIT_CWD, 'package.json');
+const { gulpConfig } = require(pathToPackage);
 
-const stylesSource = 'src/css/**/*.scss';
-const stylesDestDirectory = 'static/assets/css';
-const jsSource = 'src/js/**/*.js';
-const jsDestDirectory = 'static/assets/js';
-
-function removeFilesFromDirectory(dir) {
-	console.log('invoked')
-	return function(cb) {
-		rimraf.sync(`${dir}/*`);
-		cb();
-	}
-}
-
-gulp.task('clean:styles', removeFilesFromDirectory(stylesDestDirectory));
-
-gulp.task('styles', gulp.series('clean:styles', () => {
-	return gulp.src(stylesSource)
-		.pipe(sass({
-			includePaths: ['node_modules'],
-			outputStyle: 'compressed'
-		}).on('error', sass.logError))
-		.pipe(autoprefixer())
-		.pipe(gulp.dest(stylesDestDirectory));	
-}));
-
-gulp.task('watch:styles', () => {
-	return gulp.watch(stylesSource, gulp.series('styles'));
-});
-
-gulp.task('clean:js', removeFilesFromDirectory(jsDestDirectory));
-
-gulp.task('js', gulp.series('clean:js', () => {
-	return gulp.src(jsSource)
-		.pipe(gulp.dest(jsDestDirectory));	
-}));
-
-gulp.task('watch:js', () => {
-	return gulp.watch(jsSource, gulp.series('js'));
-});
-
-gulp.task('watch', gulp.parallel('watch:styles', 'watch:js'));
-
-gulp.task('build', gulp.parallel('styles', 'js'));
-
-gulp.task('default', gulp.parallel('build', 'watch'));
+console.log(gulpConfig);
