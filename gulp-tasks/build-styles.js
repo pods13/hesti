c734605e4a7ea2d57272985082ee72e355f67202
pods13 <alta0815@yandex.ru>
2019-10-06 18:04:42 +0300

    Include projected styles in the build process

--- gulp-tasks/build-styles.js
+++ gulp-tasks/build-styles.js
@@ -1,18 +1,33 @@
-const path = require('path');
-module.exports = function (gulp, settings, plugins) {
-	const cssAssets = path.join('static', settings.cssAssets);
-	const pathToBaseStyleSources = settings.styleSources.base.map(file => path.join('themes', 'hesti', settings.stylesSourceDirPath, file));
+const path = require("path");
+module.exports = function(gulp, settings, plugins) {
+	const cssAssets = path.join("static", settings.cssAssets);
+	const pathToBaseStyleSources = settings.styleSources.base.map(file =>
+		path.join("themes", "hesti", settings.stylesSourceDirPath, file)
+	);
+	const pathToExtendedStyleSources = settings.styleSources.extended.map(file =>
+		path.join(settings.stylesSourceDirPath, file)
+	);
 	return function() {
-		return gulp.src(pathToBaseStyleSources)
-			.pipe(plugins.sass({
-				includePaths: ['node_modules', path.resolve('themes', 'hesti', settings.stylesSourceDirPath)]
-			}).on('error', plugins.sass.logError))
-			.pipe(plugins.rename(function(file) {
-				if (file.basename === 'main') {
-					file.basename = 'hesti';
-				}
-			}))
-			.pipe(plugins.rename({dirname: ''}))
+		return gulp
+			.src(pathToExtendedStyleSources.concat(pathToBaseStyleSources))
+			.pipe(
+				plugins
+					.sass({
+						includePaths: [
+							"node_modules",
+							path.resolve("themes", "hesti", settings.stylesSourceDirPath)
+						]
+					})
+					.on("error", plugins.sass.logError)
+			)
+			.pipe(
+				plugins.rename(function(file) {
+					if (file.basename === "main") {
+						file.basename = "hesti";
+					}
+				})
+			)
+			.pipe(plugins.rename({ dirname: "" }))
 			.pipe(gulp.dest(cssAssets));
-	}
+	};
 };
