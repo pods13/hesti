9df5ceb615010160bac7fbfc28b681765424660e
pods13 <alta0815@yandex.ru>
2019-10-06 18:11:46 +0300

    selectorsToIgnore contains list of regexp

--- gulp-tasks/minify-styles.js
+++ gulp-tasks/minify-styles.js
@@ -1,32 +1,45 @@
-const path = require('path');
+const path = require("path");
 
-const uncss = require('postcss-uncss');
-const autoprefixer = require('autoprefixer');
-const csso = require('postcss-csso');
+const uncss = require("postcss-uncss");
+const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
 
-module.exports = function (gulp, settings, plugins) {
+module.exports = function(gulp, settings, plugins) {
 	const distDir = settings.distDir;
 	const cssAssets = path.join(distDir, settings.cssAssets);
-	const styleGlob = '/**/*.css';
+	const styleGlob = "/**/*.css";
 	const styleFiles = cssAssets + styleGlob;
-	const defaultHtmlFilesToParseStyles = ['/index.html', '/404.html'];
+	const defaultHtmlFilesToParseStyles = ["/index.html", "/404.html"];
 	const htmlFilesToParseStyles = settings.htmlFilesToParseStyles || [];
-	const htmlFiles = defaultHtmlFilesToParseStyles.concat(htmlFilesToParseStyles).map(file => settings.distDir + file);
-	const defaultSelectorsToIgnore = [/.*\.ripple.*/, 
+	const htmlFiles = defaultHtmlFilesToParseStyles
+		.concat(htmlFilesToParseStyles)
+		.map(file => settings.distDir + file);
+	const defaultSelectorsToIgnore = [
+		/.*\.ripple.*/,
 		/.*\.dropdown-menu.*/,
 		/.*\.show/,
-		/.*\.toggled/, /.*\.untoggled/,
-		'#bodyClick',
+		/.*\.toggled/,
+		/.*\.untoggled/,
+		"#bodyClick",
 		/.*\.nav-open.*/
 	];
 
-
-	const ignoredSelectors = defaultSelectorsToIgnore.concat(settings.selectorsToIgnore);
+	const customSelectorsToIgnore = settings.selectorsToIgnore.map(
+		selector => new RegExp(selector)
+	);
+	const ignoredSelectors = defaultSelectorsToIgnore.concat(
+		customSelectorsToIgnore
+	);
 	return function() {
-		const uncssOptions = {htmlroot: distDir, html: htmlFiles, ignore: ignoredSelectors };
-		return gulp.src(styleFiles)
-			.pipe(plugins.postcss([ uncss(uncssOptions) ]))
-			.pipe(plugins.postcss([ autoprefixer(), csso({ comments: false }) ]))
+		const uncssOptions = {
+			htmlroot: distDir,
+			html: htmlFiles,
+			ignore: ignoredSelectors
+		};
+		return gulp
+			.src(styleFiles)
+			.pipe(plugins.postcss([uncss(uncssOptions)]))
+			.pipe(plugins.postcss([autoprefixer(), csso({ comments: false })]))
 			.pipe(gulp.dest(cssAssets));
-	}
+	};
 };
